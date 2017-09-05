a0b40af2a096f9e69fc9abed1f7bf604b695b435
L115student <L115student@L115-11-043634.bitnet.wctc.edu>
2017-09-05 12:27:06 -0500

    Trying to print employees

--- src/lab4/HumanResources.java
+++ src/lab4/HumanResources.java
@@ -5,10 +5,46 @@
  */
 package lab4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Chad Werning
  */
 public class HumanResources {
-    private String chad;
+
+    private List<lab4.Employee> employees;
+
+    public HumanResources() {
+        employees = new ArrayList();
+    }
+
+    public void hireEmployee(String firstName, String lastName, String ssn) {
+        lab4.Employee e = new lab4.Employee(firstName, lastName, ssn);
+        employees.add(e);
+        orientEmployee(e);
+    }
+
+    public void orientEmployee(Employee e) {
+        e.doFirstTimeOrientation("Cube101");
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public static void main(String[] args) {
+        HumanResources hr = new HumanResources();
+        hr.hireEmployee("John", "Doe", "333-33-3333");
+        System.out.println(hr.getEmployees());
+//        for(Employee emp : hr.getEmployees()) {
+//            System.out.println(emp);
+//        }
+
+    }
 }
