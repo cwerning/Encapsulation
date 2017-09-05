8c1c91dcb7b2e3bddb5e7e9a2da6646394ee7979
L115student <L115student@L115-11-043634.bitnet.wctc.edu>
2017-09-05 12:45:47 -0500

    Added main method to Employer

--- src/lab4/Employer.java
+++ src/lab4/Employer.java
@@ -5,10 +5,38 @@
  */
 package lab4;
 
+import lab4.solution.HrPerson;
+
 /**
  *
  * @author Chad Werning
  */
 public class Employer {
+    private HumanResources hr = new HumanResources();
+    
+ 
+    
+    public void hireEmployee(String firstName, String lastName, String ssn) {
+        hr.hireEmployee(firstName, lastName, ssn);
+      
+    }
+
+    public HumanResources getHr() {
+        return hr;
+    }
+
+    public void setHr(HumanResources hr) {
+        this.hr = hr;
+    }
+        public static void main(String[] args) {
+        HumanResources hr = new HumanResources();
+        hr.hireEmployee("Chad", "Werning", "444-44-4444");
+        hr.hireEmployee("Jim", "Mallas", "555-55-5555");
+        //System.out.println(hr.getEmployees());
+        for(Employee emp : hr.getEmployees()) {
+            System.out.println(emp.toString());
+        }
+
+    }
     
 }
