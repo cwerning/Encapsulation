939999e792fdfc891c6fac8d93ce9ba57a731b7b
L115student <L115student@L115-08-043627.bitnet.wctc.edu>
2017-09-07 12:39:21 -0500

    Fixed Date Format Method

--- src/lab2/Employee.java
+++ src/lab2/Employee.java
@@ -4,22 +4,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * In this lab focus on METHOD encapsulation and fix/add code as necessary.
- * Pay special attention to the following issues:
- *    1. Not all methods need to be public
- *    2. When methods need to be called in a certain order you can do this
- *       by creating a parent method that calls the other, helper methods.
- *    3. There is some duplicate code used that violates the D.R.Y. principle.
- *       Eliminate that by encapsulating the duplicate code in a new method
- *       and then call that method in place of the duplicate code
- *    4. All method parameters should be validated.
- * 
+ * In this lab focus on METHOD encapsulation and fix/add code as necessary. Pay
+ * special attention to the following issues: 1. Not all methods need to be
+ * public 2. When methods need to be called in a certain order you can do this
+ * by creating a parent method that calls the other, helper methods. 3. There is
+ * some duplicate code used that violates the D.R.Y. principle. Eliminate that
+ * by encapsulating the duplicate code in a new method and then call that method
+ * in place of the duplicate code 4. All method parameters should be validated.
+ *
  * Review the tips in the document "EncapCheckList.pdf" if needed.
  *
- * @author      Jim Lombardo, WCTC Instructor
- * @version     1.02
+ * @author Jim Lombardo, WCTC Instructor
+ * @version 1.02
  */
 public class Employee {
+
     private String firstName;
     private String lastName;
     private String ssn;
@@ -35,18 +34,16 @@ public class Employee {
         this.lastName = lastName;
         this.ssn = ssn;
     }
-    
+
     private void simpleDateFormatDate(String index) {
-       if(index.length() == 0){
-           throw new IllegalArgumentException("Need to add whom the employee met with");
-       }
+        if (index.length() == 0) {
+            throw new IllegalArgumentException("Need to add whom the employee met with");
+        }
         SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
         String fmtDate = sdf.format(orientationDate);
-        System.out.println(firstName + " " + lastName + index
-            + fmtDate);
-       
+
     }
-    
+
     //This is my Parent method that calls the helper methods.
     public void newEmployeeProtocol() {
         meetWithHrForBenefitAndSalryInfo();
@@ -59,15 +56,22 @@ public class Employee {
     // would only do this once, upon being hired.
     private void meetWithHrForBenefitAndSalryInfo() {
         metWithHr = true;
-        simpleDateFormatDate(" met with Hr on ");        
-        
+        DateUtilities du = new DateUtilities();
+        du.getFormattedDate(orientationDate);
+        System.out.println(firstName + " " + lastName + " met with HR on "
+                + du.getFormattedDate(orientationDate));
+
     }
 
     // Assume this must be performed first, and assume that an employee
     // would only do this once, upon being hired.:
     private void meetDepartmentStaff() {
         metDeptStaff = true;
-        simpleDateFormatDate(" met with Dept. Staff on ");
+        DateUtilities du = new DateUtilities();
+        du.getFormattedDate(orientationDate);
+        System.out.println(firstName + " " + lastName + " met with Dept. Staff on "
+                + du.getFormattedDate(orientationDate));
+
     }
 
     // Assume this must be performed third. And assume that because department
@@ -75,22 +79,27 @@ public class Employee {
     // independently from other classes.
     public void reviewDeptPolicies() {
         reviewedDeptPolicies = true;
-        simpleDateFormatDate(" reviewed Dept policies on ");
+        DateUtilities du = new DateUtilities();
+        du.getFormattedDate(orientationDate);
+        System.out.println(firstName + " " + lastName + " reviewed Dept Policies on "
+                + du.getFormattedDate(orientationDate));
+
     }
 
     // Assume this must be performed 4th. And assume that because employees
     // sometimes change office locations that this method may need to be called 
     // independently from other classes.
     public void moveIntoCubicle(String cubeId) {
-        if(cubeId.length() == 0){
+        if (cubeId.length() == 0) {
             throw new IllegalArgumentException("Employee Needs a cube ID");
         }
         this.cubeId = cubeId;
         this.movedIn = true;
-        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
-        String fmtDate = sdf.format(orientationDate);        
+        DateUtilities du = new DateUtilities();
+        du.getFormattedDate(orientationDate);
         System.out.println(firstName + " " + lastName + " moved into cubicle "
-                + cubeId + " on " + fmtDate);
+                + cubeId + " on " + du.getFormattedDate(orientationDate));
+
     }
 
     public String getFirstName() {
@@ -99,12 +108,11 @@ public class Employee {
 
     // setter methods give the developer the power to control what data is
     // allowed through validation.
-    
     public void setFirstName(String firstName) {
-         if(firstName.length() == 0){
+        if (firstName.length() == 0) {
             throw new IllegalArgumentException("Employee Needs a first name");
         }
-       this.firstName = firstName;
+        this.firstName = firstName;
     }
 
     public String getLastName() {
@@ -112,10 +120,10 @@ public class Employee {
     }
 
     public void setLastName(String lastName) {
-         if(lastName.length() == 0){
+        if (lastName.length() == 0) {
             throw new IllegalArgumentException("Employee Needs a last name");
         }
-       this.lastName = lastName;
+        this.lastName = lastName;
     }
 
     public String getSsn() {
@@ -123,7 +131,7 @@ public class Employee {
     }
 
     public void setSsn(String ssn) {
-         if(ssn.length() == 0){
+        if (ssn.length() == 0) {
             throw new IllegalArgumentException("Employee Needs a ssn");
         }
         this.ssn = ssn;
@@ -135,7 +143,7 @@ public class Employee {
 
     // boolean parameters need no validation
     public void setMetWithHr(boolean metWithHr) {
-         if(metWithHr != true || metWithHr != false){
+        if (metWithHr != true || metWithHr != false) {
             throw new IllegalArgumentException("metWithHR needs to be true or false");
         }
         this.metWithHr = metWithHr;
@@ -146,7 +154,7 @@ public class Employee {
     }
 
     public void setMetDeptStaff(boolean metDeptStaff) {
-         if(metDeptStaff != true || metDeptStaff != false){
+        if (metDeptStaff != true || metDeptStaff != false) {
             throw new IllegalArgumentException("metDeptStaff needs to be true or false");
         }
         this.metDeptStaff = metDeptStaff;
@@ -157,7 +165,7 @@ public class Employee {
     }
 
     public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
-         if(reviewedDeptPolicies != true || reviewedDeptPolicies != false){
+        if (reviewedDeptPolicies != true || reviewedDeptPolicies != false) {
             throw new IllegalArgumentException("reviewedDeptPolicies  needs to be true or false");
         }
         this.reviewedDeptPolicies = reviewedDeptPolicies;
@@ -168,7 +176,7 @@ public class Employee {
     }
 
     public void setMovedIn(boolean movedIn) {
-         if(movedIn != true || movedIn != false){
+        if (movedIn != true || movedIn != false) {
             throw new IllegalArgumentException("reviewedDeptPolicies  needs to be true or false");
         }
         this.movedIn = movedIn;
@@ -178,11 +186,10 @@ public class Employee {
         return cubeId;
     }
 
-    
     public void setCubeId(String cubeId) {
-        if(cubeId.length() == 0){
-           throw new IllegalArgumentException("Need to enter cube ID");
-       }
+        if (cubeId.length() == 0) {
+            throw new IllegalArgumentException("Need to enter cube ID");
+        }
         this.cubeId = cubeId;
     }
 
@@ -191,8 +198,9 @@ public class Employee {
     }
 
     public void setOrientationDate(Date orientationDate) {
-        if(orientationDate == null){
-           throw new IllegalArgumentException("Need to enter orentation date");
-       }
+        if (orientationDate == null) {
+            throw new IllegalArgumentException("Need to enter orentation date");
+        }
         this.orientationDate = orientationDate;
-    }}
+    }
+}
